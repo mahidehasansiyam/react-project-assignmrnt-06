34f4b7546b55e8dd9c030b925ef8d38004747862
Mahedi <mahidehasansiyam123@gmail.com>
2026-03-30 22:14:27 +0600

    setup environment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-blue-700 p-5 text-2xl'>Assignment 06</h1>
+      <p>Paragraph</p>
+      <button className='btn btn-accent'>Click here </button>
+    </>
+  )
+}
+
+export default App
